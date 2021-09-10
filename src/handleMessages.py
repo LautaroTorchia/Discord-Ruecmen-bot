9aa3021a072a820aa47d5de0bf9cd57f15dee2e1
LautaroTorchia <torchia.lautaro2@gmail.com>
2021-09-09 22:51:32 -0300

    Primera funcionalidad de match history por user_id(refactorizar en algun momento)

--- src/handleMessages.py
+++ src/handleMessages.py
@@ -32,9 +32,30 @@ async def handleRecmanChange(m,bot):
             
 
 async def handleLolInfo(m,region,summoner):
+    
+    formated_data=""
+
     cassiopeia.set_riot_api_key(os.getenv("RIOT_API_KEY"))
     my_summoner=Summoner(name=summoner,region=region)
-    ultimas5=my_summoner.match_history[:5]
-    print(ultimas5)
+    last_5=my_summoner.match_history[:5]
+    games_data=list(map(lambda x:{"blue":x.blue_team,"red":x.red_team,"duration":x.duration,"id":x.id},last_5))
+    games_data=list(map(lambda x:{"blue":x["blue"].participants,"red":x["red"].participants,"duration":x["duration"],"id":x["id"]},games_data))
+    for i in games_data:
+        red_team_data=""
+        blue_team_data=""
+        for x in i["red"]:
+            red_team_data+=f"{x.summoner.name}({x.champion.name})  " 
+        for x in i["blue"]:
+            blue_team_data+=f"{x.summoner.name} ({x.champion.name})  "
+        formated_data+=f"""
+Blue side: {blue_team_data} 
+
+Red side: {red_team_data}  
+
+duracion:{i["duration"]}  game_id:{i["id"]}
+
+        """
+    await m.channel.send(formated_data)
+    
 
 
